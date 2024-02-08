0f28a14f78316ee3e5151d2455a31069ee768845
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-02-08 15:51:30 +0900

    2024.02.08 6th 무한 스크롤 페이지 네이션

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -5,10 +5,11 @@ export default {
   data() {
     return {
       itemList: [],
-      pageSize: 10,
+      pageSize: 5,
       currentPage: 0,
-      searchType:"name",
-      searchValue:``
+      searchType: "name",
+      searchValue: ``,
+      isLastPage: false,
     }
   },
 
@@ -20,9 +21,14 @@ export default {
           size: this.pageSize,
           [this.searchType]: this.searchValue,
         }
-
+        console.log("data 호출");
         const res = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/items`, {params});
-        this.itemList = res.data;
+        const addItemList = res.data
+        if (addItemList.length < this.pageSize){
+          this.isLastPage = true;
+        }
+
+        this.itemList = [...this.itemList, ...addItemList];
       } catch (e) {
         console.log(e);
       }
@@ -30,7 +36,17 @@ export default {
 
     getImage(id) {
       return `${process.env.VUE_APP_API_BASE_URL}/item/${id}/image`;
-    }
+    },
+
+    scrollPagination() {
+      // innerHeight : 브라우저 창의 내부톺이를 픽셀단위로 변환
+      // offsetHeight : 전체 브라우저 창의 높이
+      const nearBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 50;
+      if (nearBottom && !this.isLastPage) {
+        this.currentPage++;
+        this.fetchItems();
+      }
+    },
 
   },
 
@@ -38,20 +54,22 @@ export default {
     this.fetchItems();
   },
 
-
+  mounted() {
+    window.addEventListener('scroll', this.scrollPagination);
+  },
 }
-
-
 </script>
+
+
 
 <template>
   <div class="container">
     <div class="page-header" style="padding: 15px">
       <h1>상품 목록</h1>
       <div class="d-flex justify-content-between" style="margin-top:20px">
-        <form @submit.prevent="fetchItems" style="display: flex; padding : 10px">
-          <select class="form-control"
-                  v-model="searchType"
+        <form style="display: flex; padding : 10px" @submit.prevent="fetchItems">
+          <select v-model="searchType"
+                  class="form-control"
                   style="display: inline-block;
                   width: auto;
                   margin-right: 5px">
@@ -59,8 +77,8 @@ export default {
             <option value="name">상품명</option>
             <option value="category">카테고리</option>
           </select>
-          <input type="text" class="form-control" v-model="searchValue" placeholder="텍스트 입력" />
-          <button class="btn" type="submit" style="margin-left: 5px"> 제출 </button>
+          <input v-model="searchValue" class="form-control" placeholder="텍스트 입력" type="text"/>
+          <button class="btn" style="margin-left: 5px" type="submit"> 제출</button>
         </form>
       </div>
 
